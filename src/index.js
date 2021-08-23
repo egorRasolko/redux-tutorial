9d12c97c7716abdaf35e44c4187748e0096e5ffe
egorRasolko <83365207+egorRasolko@users.noreply.github.com>
2021-08-23 18:57:18 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {store} from './store/configureStore';
+import App from './containers/App';
+
+import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
